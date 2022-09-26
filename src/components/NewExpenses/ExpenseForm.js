3547bcbdbd6326563ec25045a0f1f052c1080619
Madeline Yi <sunhunfunbun@gmail.com>
2022-09-25 22:15:51 -0500

    created several state variables

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react'; 
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-    
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
     const titleChangeHandler = (event) => {
-        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
     }
 
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
 
     return <form>
         <div className='new-expense__controls'>
@@ -16,12 +26,12 @@ const ExpenseForm = () => {
 
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type ='number' min="0.01" step="0.01" />
+                <input type ='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
             </div>
 
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type ='date' min="2019-01-01" max="2024-01-01" />
+                <input type ='date' min="2019-01-01" max="2024-01-01" onChange={dateChangeHandler}/>
             </div>
         </div>
 
